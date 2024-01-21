1d4ece95876f6f981c0f98c236b665d403f83272
rutvikpatel14 <rutvikpatel7534@gmail.com>
2024-01-21 18:07:00 +0530

    Port change

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -1,7 +1,7 @@
 import socketIO from "socket.io-client";
 
 export let socket;
-export const ENDPOINT = "http://localhost:4000/";
+export const ENDPOINT = process.env.PORT;
 
 export const useSocket = () => {
   const connect = () => {
